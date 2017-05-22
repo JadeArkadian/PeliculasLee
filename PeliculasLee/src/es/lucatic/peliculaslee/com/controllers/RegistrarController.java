50836ed25ed582efe9c7cc81cac49cce1d3a1d9f
meriiis <admin@pwc1>
2017-05-22 16:07:23 +0200

    Añadido Registrar Controller, spring-servlet.xml y web.xml.

---
+++ PeliculasLee/src/es/lucatic/peliculaslee/com/controllers/RegistrarController.java
@@ -0,0 +1,12 @@
+package es.lucatic.peliculaslee.com.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RegistrarController {
+
+	public RegistrarController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
