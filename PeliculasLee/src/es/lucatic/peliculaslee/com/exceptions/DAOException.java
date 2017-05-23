06ee642648b3cfd53f47892446463e6fce04e760
Daniel <danielballesteros4@gmail.com>
2017-05-23 14:10:27 +0200

    Querys and Exceptions

---
+++ PeliculasLee/src/es/lucatic/peliculaslee/com/exceptions/DAOException.java
@@ -0,0 +1,29 @@
+package es.lucatic.peliculaslee.com.exceptions;
+
+public class DAOException extends RuntimeException {
+
+	public DAOException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DAOException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DAOException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DAOException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DAOException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
